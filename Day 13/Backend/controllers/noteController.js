e2ca8430f691dbdeedc73235eb6af7f703a1adf9
Amriita04 <167612045+Amriita04@users.noreply.github.com>
2025-08-29 13:31:14 +0530

    Add files via upload

---
+++ Day 13/Backend/controllers/noteController.js
@@ -0,0 +1,23 @@
+const Note = require('../models/Note');
+
+exports.getNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.json(notes);
+};
+
+exports.addNote = async (req, res) => {
+  const newNote = new Note(req.body);
+  await newNote.save();
+  res.json(newNote);
+};
+
+exports.updateNote = async (req, res) => {
+  const updated = await Note.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updated);
+};
+
+exports.deleteNote = async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Note deleted' });
+};
+
