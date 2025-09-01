130c49338984f18c9a78720b48d92a2a22b7b8b2
Nisha-147 <mewadanisha028@gmail.com>
2025-09-01 20:44:47 +0530

    Add files via upload

---
+++ Day 6/src/App.jsx
@@ -0,0 +1,18 @@
+import ProfileCard from './profilecard';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <ProfileCard
+        name="Amrita Pal"
+        avatar="https://randomuser.me/api/portraits/women/1.jpg"
+        bio="AI model developer. AI enthusiast. Car lover."
+      />
+    </div>
+  );
+}
+
+
+
+export default App;
